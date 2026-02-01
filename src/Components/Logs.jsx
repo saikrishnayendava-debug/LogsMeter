b9d12159ac8cc69c4acee6d7c71364ae2e32eb4f
TARUN SAI  KRISHNA Y <143386989+tarunsaiy@users.noreply.github.com>
2026-02-01 15:50:49 +0530

    Refactor ActiveUsers component to Logs component

--- src/Components/Logs.jsx
+++ src/Components/Logs.jsx
@@ -2,27 +2,25 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import Loader from './Loader';
-const ActiveUsers = () => {
+const Logs = () => {
     const [page, setPage] = useState(1);
-    const [limit] = useState(50);
-    const [activeUsers, setActiveUsers] = useState([]);
-    const [activeUsersCount, setActiveUsersCount] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit] = useState(100);
+    const [logs, setLogs] = useState([]);
+    const [logsCount, setLogsCount] = useState(0);
     const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState("");
-    const handleActiveUsers = async () => {
+    const handleLogs = async () => {
         try {
             setLoading(true);
-            const response = await axios.get('https://database-9qqy.onrender.com/totalUsers', { params: { page, limit, search } });
+            console.log(page + "page inside")
+            const response = await axios.get('https://database-9qqy.onrender.com/getLogs', { params: { page, limit, search } });
             if (response.data.success) {
-                setActiveUsers(response.data.totalUsers);
-                setActiveUsersCount(response.data.count);
+                setLogs(response.data.logs);
+                setLogsCount(response.data.count);
             }
         } catch (error) {
-            return response.status(500).json({
-                success: false,
-                message: 'Error fetching active users',
-                error: error.message,
-            });
+            console.log(error);
         }
         finally {
             setLoading(false);
@@ -30,56 +28,66 @@ const ActiveUsers = () => {
     }
     useEffect(() => {
         const delay = setTimeout(() => {
-            handleActiveUsers();
+            handleLogs();
         }, 200);
 
         return () => clearTimeout(delay);
     }, [page, search])
     return (
         <>
-            <div className='flex justify-evenly mt-5'>
+            <div className='flex justify-evenly mt-5 text-slate-200'>
                 <button className='border border-[#222528] rounded w-25 text-sm font-bold' onClick={() => setPage((prev) => Math.max(1, prev - 1))} >previous</button>
                 <p className='border border-[#222528] rounded w-fit px-4 text-sm font-bold'>page: {page}</p>
-
-                <p className='border border-[#222528] rounded w-25 text-sm font-bold'>count {activeUsersCount}</p>
-                <button className='border border-[#222528] rounded w-25 text-sm font-bold' onClick={() => setPage((prev) => Math.min(prev + 1, Math.ceil(activeUsersCount / limit)))}>next</button>
-
-
+                <p className='border border-[#222528] rounded w-fit px-4 text-sm font-bold'>count: {logsCount}</p>
+                <button className='border border-[#222528] rounded w-25 text-sm font-bold' onClick={() => setPage((prev) => Math.min(prev + 1, Math.ceil(logsCount / limit)))}>next</button>
             </div>
             <div className='flex justify-center items-center my-4'>
-                <input type="text" className='bg-black border border-[#222528] rounded-md  outline-none text-slate-200 p-2' onChange={(e) => setSearch(e.target.value)}/>
+                <input type="text" className=' bg-black border border-[#222528] rounded-md  outline-none text-slate-200 p-2' onChange={(e) => setSearch(e.target.value)} />
             </div>
-                
             {
                 loading ? (
                     <div style={{ display: "flex", justifyContent: "center", marginTop: "40px" }}>
                         <Loader />
                     </div>
-                ) : (
-                    <table className="table-auto border-collapse border border-[#222528] w-full text-sm mt-5">
-                        <thead>
-                            <tr className="bg-white text-slate-900">
-                                <th className="border border-[#222528] px-2 py-1">S.No</th>
-                                <th className="border border-[#222528] px-2 py-1">Number</th>
-                                <th className="border border-[#222528] px-2 py-1">Time</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {activeUsers.map((user, index) => (
-                                <tr key={user._id}>
-                                    <td className="border border-[#222528] px-2 py-1 bg-[#0a0a0a] text-slate-200">{index + 1}</td>
-                                    <td className="border border-[#222528] px-2 py-1 bg-[#0a0a0a] text-slate-200">{user.number}</td>
-                                    <td className="border border-[#222528] px-2 py-1 bg-[#0a0a0a] text-slate-200">{user.time}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
                 )
+                    : (
+                        <table className="table-auto border-collapse border border-[#222528] w-full text-sm">
+                            <thead>
+                                <tr className="bg-white text-slate-900 font-bold">
+                                    <th className="border  px-2 py-1">S.No</th>
+                                    <th className="border  px-2 py-1">Number</th>
+                                    <th className="border  px-2 py-1">Time</th>
+                                    <th className="border  px-2 py-1">Server</th>
+
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {logs.map((user, index) => (
+                                    <tr
+                                        key={user._id}
+                                        className={
+                                            user.status === 500
+                                                ? "bg-red-500/30 text-white"
+                                                : user.status === 300
+                                                    ? "bg-yellow-400 text-white"
+                                                    : ""
+                                        }
+                                    >
+                                        <td className="border border-[#222528] px-2 py-1 bg-[#0a0a0a] text-slate-200">{index + 1}</td>
+                                        <td className="border border-[#222528] px-2 py-1 bg-[#0a0a0a] text-slate-200">{user.number}</td>
+                                        <td className="border border-[#222528] px-2 py-1 bg-[#0a0a0a] text-slate-200">{user.time}</td>
+                                        <td className="border border-[#222528] px-2 py-1 bg-[#0a0a0a] text-slate-200">{user.server}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )
             }
+
 
         </>
     )
 }
 
-export default ActiveUsers
+export default Logs
