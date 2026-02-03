e0b5c5be024aa0f10d3e3a77b2bb5d6a4cb9c1d8
saikrishnayendava-debug <saikrishnayendava@gmail.com>
2026-02-03 14:20:22 +0530

    CHANGEd UI

--- src/App.jsx
+++ src/App.jsx
@@ -40,14 +40,14 @@ function App() {
   return (
     <div className='bg-black text-slate-200 h-screen text-sm font-extrabold'>
       <div className='w-full flex justify-around py-2 border-b border-[#222528]'>
-        <button className='bg-[#03ff81] text-black  w-30 rounded-2xl py-3 ' onClick={handleLogs}>Logs</button>
-        <button className='bg-[#03ff81] text-black w-30 rounded-2xl py-3' onClick={handleActiveUsers}>Active Users</button>
+        <button className='bg-[#222528] text-white  w-30 rounded-2xl py-3 ' onClick={handleLogs}>Logs</button>
+        <button className='bg-[#222528] text-white w-30 rounded-2xl py-3' onClick={handleActiveUsers}>Active Users</button>
       </div>
       {
         logs ? <Logs/> : <ActiveUsers/>
       }
       <div className='fixed bottom-0 flex justify-center w-full py-1'>
-        <button className='bg-slate-800 text-white py-3 px-6 rounded-2xl ' onClick={() => {setOpen(true)}}>delete logs</button>
+        <button className='bg-red-900 text-white py-3 px-6 rounded-2xl ' onClick={() => {setOpen(true)}}>delete logs</button>
       </div>
       {
         open && (<CofirmBox close={() => setOpen(false)} handleDelete={handleDelete}/>)
--- src/Components/Logs.jsx
+++ src/Components/Logs.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+import { FaAngleDoubleLeft } from "react-icons/fa";
+import { FaAngleDoubleRight } from "react-icons/fa";
 import Loader from './Loader';
 const Logs = () => {
     const [page, setPage] = useState(1);
@@ -14,7 +16,7 @@ const Logs = () => {
     const handleLogs = async () => {
         try {
             setLoading(true);
-            console.log(page + "page inside")
+            
             const response = await axios.get('https://database-9qqy.onrender.com/getLogs', { params: { page, limit, search } });
             if (response.data.success) {
                 setLogs(response.data.logs);
@@ -47,14 +49,14 @@ const Logs = () => {
     return (
         <>
             <div className='flex justify-evenly mt-5 text-slate-200'>
-                <button className='border border-[#222528] rounded w-25 text-sm font-bold' onClick={() => setPage((prev) => Math.max(1, prev - 1))} >previous</button>
-                <p className='border border-[#222528] rounded w-fit px-4 text-sm font-bold'>page: {page}</p>
-                <p className='border border-[#222528] rounded w-fit px-4 text-sm font-bold'>count: {logsCount}</p>
-                <button className='border border-[#222528] rounded w-25 text-sm font-bold' onClick={() => setPage((prev) => Math.min(prev + 1, Math.ceil(logsCount / limit)))}>next</button>
+                <button className='border border-[#222528] rounded px-5 py-2 text-sm font-bold' onClick={() => setPage((prev) => Math.max(1, prev - 1))} ><FaAngleDoubleLeft size={20}/></button>
+                <p className='border border-[#222528] rounded w-fit px-3 py-2 text-sm font-bold'>page: {page}</p>
+                <p className='border border-[#222528] rounded w-fit px-5 py-2 text-sm font-bold'>count: {logsCount}</p>
+                <button className='border border-[#222528] rounded px-5 py-2 text-sm font-bold' onClick={() => setPage((prev) => Math.min(prev + 1, Math.ceil(logsCount / limit)))}><FaAngleDoubleRight size={20}/></button>
             </div>
             <div className='flex justify-center items-center gap-5 my-4'>
                 <input type="text" className=' bg-black border border-[#222528] rounded-md  outline-none text-slate-200 p-2' onChange={(e) => setSearch(e.target.value)} />
-                <div className={`bg-slate-800 text-white py-3 rounded-2xl w-20 text-center ${live && "animate-pulse "}`} onClick={handleLive}>Live</div>
+                <div className={`bg-white text-slate-800 py-3 rounded-2xl w-20 text-center ${live && "animate-pulse "}`} onClick={handleLive}>Live</div>
             </div>
             {
                 loading ? (
@@ -78,11 +80,11 @@ const Logs = () => {
                                                     : "bg-[#0a0a0a] text-slate-200"
                                         }
                                     >
-                                        <td className="border border-[#222528] px-2 py-1 ">{index + 1}</td>
-                                        <td className="border border-[#222528] px-2 py-1 ">{user.number}</td>
-                                        <td className="border border-[#222528] px-2 py-1 ">{user.time}</td>
-                                        <td className={`border border-[#222528] px-2 py-1 ${user.response <= 5 && "bg-green-500 text-black"} `}>{user.response}</td>
-                                        <td className="border border-[#222528] px-2 py-1 ">{user.server}</td>
+                                        <td className="border border-[#222528] px-2 py-1 text-center">{index + 1}</td>
+                                        <td className="border border-[#222528] px-2 py-1 text-center">{user.number}</td>
+                                        <td className="border border-[#222528] px-2 py-1 text-center">{user.time}</td>
+                                        <td className={`border border-[#222528] px-2 py-1 ${user.response <= 5 && "bg-green-500 text-black"} text-center`}>{user.response}</td>
+                                        <td className={`border border-[#222528] px-2 py-1 text-center ${user.server === 2 && "bg-purple-950"}`}>{user.server}</td>
                                     </tr>
                                 ))}
                             </tbody>
